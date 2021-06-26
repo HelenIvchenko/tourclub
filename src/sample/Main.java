27a6c2a86473a67e26c2f0d83764e6ea3672e05f
Olena Ivchenko <helen.ivchenko@gmai.com>
2021-06-26 18:45:56 +0300

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,28 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Tour Club \"Hiking with Olena\"");
+        Button button = new Button("This button does nothing");
+        HBox hbox = new HBox(button);
+        primaryStage.setScene(new Scene(hbox, 800, 600));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
